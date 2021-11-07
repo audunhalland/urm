677d2bd8d51fe515157e4de8657a85aa3bd2c377
Audun Halland <audun.halland@pm.me>
2021-11-07 12:10:05 +0100

    Unary, docs, etc

--- urm/src/func.rs
+++ urm/src/func.rs
@@ -4,21 +4,36 @@
 
 use crate::build::Build;
 use crate::builder::QueryBuilder;
-use crate::ty::{Type, Typed, Unit};
+use crate::ty::{Nullable, Type, Typed};
 use crate::Database;
 
-pub trait Unary<T> {
+/// A unary function, accepting one parameter/operand
+pub trait Unary<DB: Database, T>: Send + Sync + 'static {
     type Output: Type;
+
+    fn build(&self, builder: &mut QueryBuilder<DB>, operand: &T);
 }
 
 impl<DB, Op, T> Typed<DB> for (Op, T)
 where
     DB: Database,
-    Op: Unary<T>,
+    Op: Unary<DB, T>,
 {
     type Ty = Op::Output;
 }
 
+impl<DB, Op, T> Build<DB> for (Op, T)
+where
+    DB: Database,
+    Op: Unary<DB, T>,
+    T: Build<DB>,
+{
+    fn build(&self, builder: &mut QueryBuilder<DB>) {
+        self.0.build(builder, &self.1);
+    }
+}
+
+/// A binary function, accepting two parameters/operands
 pub trait Binary<DB: Database, L, R>: Send + Sync + 'static {
     type Output: Type;
 
@@ -36,32 +51,27 @@ where
 impl<DB, Op, L, R> Build<DB> for (Op, L, R)
 where
     DB: Database,
+    Op: Binary<DB, L, R>,
     L: Build<DB>,
     R: Build<DB>,
-    Op: Binary<DB, L, R>,
 {
     fn build(&self, builder: &mut QueryBuilder<DB>) {
         self.0.build(builder, &self.1, &self.2)
     }
 }
 
+/// Binary function that tests equality between two given operands
 #[derive(Debug)]
-pub struct Equals<DB>(std::marker::PhantomData<DB>);
+pub struct Equals;
 
-impl<DB> Equals<DB> {
-    pub fn new() -> Self {
-        Self(std::marker::PhantomData)
-    }
-}
-
-impl<DB, L, R> Binary<DB, L, R> for Equals<DB>
+impl<DB, L, R> Binary<DB, L, R> for Equals
 where
     DB: Database,
     L: Build<DB>,
     R: Build<DB>,
     L::Ty: Type<Output = <R::Ty as Type>::Output>,
 {
-    type Output = Unit<bool>;
+    type Output = Nullable<bool>;
 
     fn build(&self, builder: &mut QueryBuilder<DB>, left: &L, right: &R) {
         left.build(builder);
